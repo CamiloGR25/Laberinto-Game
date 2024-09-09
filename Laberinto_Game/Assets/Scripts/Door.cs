4dd7fd77dbbedaec1eeecda910073692a391a9fe
Camilo <camilogomezr25@gmail.com>
2024-09-09 11:57:26 -0500

    Crear la funcionabilidad de que se abre la puerta cuando se recoje la llave

---
+++ Laberinto_Game/Assets/Scripts/Door.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private AudioSource puerta;
+    void Start()
+    {
+        puerta = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            puerta.Play();
+        }
+    }
+}
---
+++ Laberinto_Game/Assets/Scripts/Key.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    private int velocidadRotacion = 170;
+    private AudioSource PuertaAbiertaSonido;
+    void Start()
+    {
+        PuertaAbiertaSonido = GameObject.Find("Explosion Sonido").GetComponent<AudioSource>();//traer el componente por nombre del objeto
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.SetActive(false);
+            AudioSource.PlayClipAtPoint(PuertaAbiertaSonido.clip, transform.position);
+            GameObject puerta = GameObject.FindWithTag("Puerta");
+            puerta.SetActive(false);
+            GameObject luz = GameObject.Find("Luz Llave");
+            luz.SetActive(false);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back, velocidadRotacion * Time.deltaTime); //rotar el objeto
+    }
+}
